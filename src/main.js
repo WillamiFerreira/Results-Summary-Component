7bef40186afed5b90579563e6192c3935a94ebbb
Gabriel Ferreira <willamigabrielf.souza@gmail.com>
2023-04-14 08:32:05 -0300

    done

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import data from '../data/data.json' assert {type: 'json'};
+
+for (let i = 0; i < 4; i++){
+    let icon_and_title_ct = document.querySelectorAll('#icon_and_title')[i];
+    let score = document.querySelectorAll("#score")[i];
+
+    let item_icon = document.createElement('img');
+    let item_category = document.createElement('p');
+    let item_score = document.createElement('p');
+
+    item_icon.src = data[i]['icon'];
+    item_category.innerHTML = data[i]['category'];
+    item_score.innerText = data[i]['score'] + ' / 100';
+
+    icon_and_title_ct.appendChild(item_icon);
+    icon_and_title_ct.appendChild(item_category);
+    score.appendChild(item_score);
+
+
+}
